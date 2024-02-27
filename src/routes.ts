e47ba1ae23210fcde984ec19635259026b719ed3
Amine Mbarki <aminembarki@gmail.com>
2024-02-27 18:01:09 +0100

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,40 @@
+import { Dataset, createPlaywrightRouter } from 'crawlee';
+
+export const router = createPlaywrightRouter();
+
+router.addDefaultHandler(async ({ request, page, log }) => {
+
+    const url = request.url;
+    log.debug(`Handling request for ${url}`);
+
+    // wait page to load 
+    await page.waitForLoadState('networkidle', { timeout: 0 });
+ 
+    const title = await page.title();
+
+    // get jobResults li elements and extract the job title and job url and push to jobs array 
+    const jobResults =  () => {
+        const jobs:any = [];
+        const jobList = document.querySelectorAll('#search-results-list li');
+        jobList.forEach((job) => {
+            const jobTitle = (job.querySelector('h3') as HTMLHeadingElement)?.textContent;
+            const jobUrl = (job.querySelector('a') as HTMLAnchorElement)?.href;
+            jobs.push({jobTitle, jobUrl});
+        });
+        return jobs;
+    }
+
+
+    const jobs = await page.evaluate(jobResults);
+
+    await Dataset.pushData({
+        url,
+        title,
+        jobs : jobs
+    });
+
+    log.debug(`Pushed data for ${url}`);
+
+
+});
+
